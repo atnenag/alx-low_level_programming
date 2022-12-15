ced1e9330a08d2abdbd9907392920a91a5069926
Atnafu <atnenag@gmail.com>
2022-12-15 06:26:36 -0800

    new  second part

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Prints "_putchar" followed by a new line.
- *
- * Return: Always 0.
- */
+  * main -Entry point
+  * Return: Always 0
+  */
+
 int main(void)
 {
 	_putchar('_');
