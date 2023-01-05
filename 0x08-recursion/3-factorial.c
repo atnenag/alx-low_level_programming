d124a9ed6b8db0ec3e3a4091f62410b801daa3ea
Atnafu <atnenag@gmail.com>
2023-01-05 06:54:39 -0800

    recusiv two assignment three

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int factorial(int n) is the fanction that return the factorial of given no n
+ * @n: integer
+ * Return: factorial value of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
