eab3740882bda63d0aebc89684e612666251ce47
Atnafu <atnenag@gmail.com>
2023-01-29 03:56:39 -0800

    Debugging assinment re check result

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,7 +10,17 @@ int main(void)
 	int i;
 
 	i = 0;
-	positive_or_negative(i);
+	/*positive_or_negative(i);*/
+	if (i > 0) 
+	{
+		printf("%d is positive",i);
+	}
+	else if (i < 0)
+	{
+		 printf("%d is negative",i);
+	}
+	else 
+		 printf("%d is zero",i);
 
 	return (0);
 }
